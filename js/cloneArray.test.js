d8edf39cd5f96aaf3350ab58566f2528fdbb4715
John Welty <john@weltyautomation.com>
2022-04-23 09:19:33 -0500

    Remove learning tests and samples

--- js/cloneArray.test.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@jest/globals');
-const cloneArray = require('./cloneArray');
-
-test('properly clones array', () => {
-    const array = [1, 3, 4];
-    expect(cloneArray(array)).toEqual(array);
-    expect(cloneArray(array)).not.toBe(array);
-})
