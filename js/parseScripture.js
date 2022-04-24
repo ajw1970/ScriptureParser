c53d773920a5ad7198b091fc96b9de50c87bb0b0
John Welty <john@weltyautomation.com>
2022-04-24 07:49:01 -0500

    add structure to define intent

--- js/parseScripture.js
+++ js/parseScripture.js
@@ -1,7 +1,23 @@
 function parseScripture(quote) {
+    const data = {
+        book: "",
+        chapter: "",
+        verseRange: "",
+        text: quote
+    }
+
     const match = quote.match(/^(.*?) (\d+):(\d+) (.*)$/);
-    match.shift();
-    return match;
+    
+    switch (match.length) {
+        case 5:
+            data.book = match[1];
+            data.chapter = match[2];
+            data.verseRange = match[3];
+            data.text = match[4];
+            break;
+    }
+    
+    return Object.values(data);
 }
 
 module.exports = parseScripture;
