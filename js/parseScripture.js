42c8b880f32d96757411773ad2a6b9f34b8909a2
John Welty <john@weltyautomation.com>
2022-04-24 13:41:08 -0500

    We still need to update verseRange property

--- js/parseScripture.js
+++ js/parseScripture.js
@@ -6,7 +6,7 @@ function parseScripture(quote) {
         text: quote
     }
 
-    const match = quote.match(/^(?<book>.*?) (?<chapter>\d+):(?<verseRange>\d+) (?<text>.*)$/);
+    var match = quote.match(/^(?<book>.*?) (?<chapter>\d+):(?<verseRange>\d+) (?<text>.*)$/);
     
     switch (match.length) {
         case 5:
@@ -15,6 +15,13 @@ function parseScripture(quote) {
                 chapter: match.groups.chapter,
                 verseRange: match.groups.verseRange,
                 text: match.groups.text
+            }
+
+            //look for more than one verse in this chapter
+            const regExp = new RegExp(` ${data.chapter}:(\\d+) `, 'g');
+            if (regExp.test(data.text)) {
+                data.text = data.text.replace(regExp, '\n$1 ');
+                data.text = data.verseRange + ' ' + data.text;
             }
             break;
     }
